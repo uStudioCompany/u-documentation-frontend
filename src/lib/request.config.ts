3188f54d314c42743b740d67c7c2b104a7fd4492
drizzer14 <drizzer14@gmail.com>
2020-04-29 16:50:31 +0300

    refactor: refactor request.config

--- src/lib/request.config.ts
+++ src/lib/request.config.ts
@@ -3,13 +3,15 @@ import { AxiosRequestConfig } from 'axios';
 const serviceProtocol = 'http';
 const serviceAddress = '185.25.116.133';
 const servicePort = 3535;
-const serviceURL = `${serviceProtocol}://${serviceAddress}:${servicePort}`;
+const serviceUrl = `${serviceProtocol}://${serviceAddress}${servicePort ? `:${servicePort}` : ''}`;
+
+const params = {
+  branch: 'develop',
+  path: 'info',
+} as const;
 
 export const getMarkdownFileConfig = (...[owner, repo, fileName]: [string, string, string]): AxiosRequestConfig => ({
   method: 'get',
-  url: `${serviceURL}/entries/${owner}/${repo}/${fileName}.md`,
-  params: {
-    branch: 'develop',
-    path: 'info',
-  },
+  url: `${serviceUrl}/entries/${owner}/${repo}/${fileName}.md`,
+  params,
 });
