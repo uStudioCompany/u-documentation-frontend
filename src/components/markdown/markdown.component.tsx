aa1f9106eda8b9d6be644cd96888057515f6c156
drizzer14 <drizzer14@gmail.com>
2020-04-29 16:21:56 +0300

    fix: fix copyLogo script

--- src/components/markdown/markdown.component.tsx
+++ src/components/markdown/markdown.component.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import { renderers } from './markdown.module';
 import { MarkdownProps } from './markdown.types';
 
 export const Markdown: React.FC<MarkdownProps> = ({ source }) => {
-  return <ReactMarkdown source={source} escapeHtml={false} />;
+  return <ReactMarkdown source={source} renderers={renderers as {}} escapeHtml={false} />;
 };
