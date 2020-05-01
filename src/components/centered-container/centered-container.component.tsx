83145f0b079c0bac1d800a27c557b5fc83159c8e
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-05-01 12:54:48 +0300

    feat: Add animations to App

---
+++ src/components/centered-container/centered-container.component.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+import Styled from './centered-container.styles';
+
+export const CenteredContainer: FC = ({ children }) => <Styled.CenteredContainer>{children}</Styled.CenteredContainer>;
---
+++ src/components/centered-container/centered-container.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const CenteredContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default { CenteredContainer };
