0619725e15eb55d8ee9f4fd00e0130a477a8e2a7
semenov-ol <semenov.os16@gmail.ocm>
2020-06-15 14:50:58 +0300

    feat: Add useRequest hook for main.component.tsx

--- src/pages/main/main.component.tsx
+++ src/pages/main/main.component.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 
 import Flex from 'ustudio-ui/components/Flex';
 import Text from 'ustudio-ui/components/Text';
 import Spinner from 'ustudio-ui/components/Spinner';
 
+import { useRequest } from 'honks';
 import { Markdown } from '../../components/markdown';
 import { CenteredContainer } from '../../components/centered-container';
 import { FadeIn } from '../../components/fade-in';
@@ -14,26 +15,14 @@ import { name } from '../../../config.json';
 import { getMarkdownDocument, Wrapper } from './main.module';
 
 export const Main = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [source, setSource] = useState('');
-  const [error, setError] = useState<string | null>(null);
+  const getMainContent = async (): Promise<string> => {
+    return getMarkdownDocument();
+  };
 
-  const getMainContent = useCallback(async (): Promise<void> => {
-    setLoading(true);
-
-    try {
-      const markdownFile = await getMarkdownDocument();
-
-      setSource(markdownFile);
-    } catch ({ message: errorMessage }) {
-      setError(errorMessage);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const { sendRequest, onSuccess, onFail, onPending } = useRequest(getMainContent);
 
   useEffect(function getSourceDataOnMount() {
-    getMainContent();
+    sendRequest();
   }, []);
 
   return (
@@ -43,31 +32,37 @@ export const Main = () => {
       </Helmet>
 
       <Wrapper>
-        {isLoading && !error && (
-          <FadeIn>
-            <CenteredContainer>
-              <Flex alignment={{ horizontal: 'center' }}>
-                <Spinner appearance={{ size: 48 }} />
-              </Flex>
-            </CenteredContainer>
-          </FadeIn>
-        )}
+        {onPending(() => {
+          return (
+            <FadeIn>
+              <CenteredContainer>
+                <Flex alignment={{ horizontal: 'center' }}>
+                  <Spinner appearance={{ size: 48 }} />
+                </Flex>
+              </CenteredContainer>
+            </FadeIn>
+          );
+        })}
 
-        {!isLoading && !error && (
-          <FadeIn>
-            <Markdown source={source} />
-          </FadeIn>
-        )}
+        {onSuccess((data) => {
+          return (
+            <FadeIn>
+              <Markdown source={data} />
+            </FadeIn>
+          );
+        })}
 
-        {!isLoading && error && (
-          <FadeIn>
-            <CenteredContainer>
-              <Text variant="h5" color="var(--c-negative)" align="center">
-                {`${error} ☹️`}
-              </Text>
-            </CenteredContainer>
-          </FadeIn>
-        )}
+        {onFail((error) => {
+          return (
+            <FadeIn>
+              <CenteredContainer>
+                <Text variant="h5" color="var(--c-negative)" align="center">
+                  {`${error} ☹️`}
+                </Text>
+              </CenteredContainer>
+            </FadeIn>
+          );
+        })}
       </Wrapper>
     </>
   );
